d1eff074556ba7ba5ffb7e54c22cb50b0d51f9de
Herman Ras <herman.ras.it@gmail.com>
2023-06-17 14:55:58 +0200

    Updated Security and Bootstrap

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 ## ADD Entry in package.json
 - "productName": "Node_ElectronDemo"
 
+## for client HTML add Meta to remove Mix Content Security warning.
+<meta http-equiv="Content-Security-Policy" content="script-src 'self'">
+
+
 ## to build install electron local, you can delete after
 npm install --save-dev electron
 npm run build
--- main.js
+++ main.js
@@ -1,16 +1,34 @@
+const isDEV = process.env.NODE_ENV !== 'production';
 const path = require('path');
 const { app, BrowserWindow } = require('electron');
 
 function createMainWindow() {
     const mainWindow = new BrowserWindow({
         title: "Main Menu",
-        width: 500,
+        width: isDEV ? 1000 : 500,
         height: 800,
     });
+
+    //if in dev .env open dev tools
+    if (isDEV) {
+        mainWindow.webContents.openDevTools();
+    }
 
     mainWindow.loadFile(path.join(__dirname, './renderer/index.html'));
 }
 
 app.whenReady().then(() => {
     createMainWindow();
+
+    app.on('active', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createMainWindow();
+        }
+    });
 });
+
+app.on('window-all-closed', () => {
+    if (process.platfrom !== 'darwin') {
+        app.quit();
+    }
+})
